780e40f25d6b525779e8bb13ebc27a64e051d5bf
nonolook <looklooknono@outlook.com>
2025-06-12 15:27:36 +0800

    [all] add third points

--- api/routers/third.ts
+++ api/routers/third.ts
@@ -1,9 +1,14 @@
 import { AppDS, tables } from "@/db";
 import express from "express";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { isAddress, type Address } from "viem";
 import { CommonResponse } from "../common";
 import { validate } from "../validator";
+import { getIndexConfig } from "@/db/help";
+import { indexEventName } from "@/schedule/index_events";
+import { getPC } from "@/lib/publicClient";
+import abiBVault from "@/configs/abiBVault";
+import _ from "lodash";
 const r = express.Router();
 export default r;
 
@@ -32,3 +37,87 @@ r.post(
     }
   }
 );
+
+const getBlockTime = async (block: bigint) => {
+  const bt = await AppDS.manager.findOne(tables.index_block_time, { where: { block } });
+  if (bt) {
+    return bt.time;
+  }
+  const b = await getPC().getBlock({ blockNumber: block });
+  return parseInt(b.timestamp.toString());
+};
+
+r.get("/checkyt/:account", validate([param("account").isEthereumAddress()]), async (req, res) => {
+  const account = req.params["account"] as Address;
+  const data = {
+    status: 0,
+    data: {
+      timestamp: 0,
+      tx: "",
+    },
+  };
+  // const deposit = await AppDS.manager.findOne(tables.event_bvault_deposit, { where: { user: account } });
+  const swap = await AppDS.manager.findOne(tables.event_bvault_swap, { where: { user: account } });
+  if (Boolean(swap)) {
+    data.status = 1;
+    data.data.timestamp = await getBlockTime(swap!.block);
+    data.data.tx = swap!.tx;
+  }
+  res.status(200).json(data);
+});
+
+r.get(`/points/:bvault/:block`, validate([param("bvault").isEthereumAddress(), param("block").isNumeric()]), async (req, res) => {
+  try {
+    const bvault = req.params["bvault"] as Address;
+    const blockNumber = BigInt(req.params["block"]);
+    const ec = await AppDS.manager.findOne(tables.index_event, { where: { address: bvault, table: "event_bvault_swap" } });
+    if (!ec || blockNumber <= ec.start) {
+      return res.status(404).send();
+    }
+    const indexBlockNumber = await getIndexConfig("index_block_time");
+    const indexEventBlock = await getIndexConfig(indexEventName(ec));
+    if (blockNumber > indexBlockNumber || blockNumber > indexEventBlock) {
+      return res.status(500).json({ code: 120, message: "Need wait server index block" });
+    }
+    const blockTime = await getBlockTime(blockNumber);
+    const pc = getPC(undefined, 1);
+    const epochCount = await pc.readContract({ abi: abiBVault, address: bvault, functionName: "epochIdCount", blockNumber });
+    if (epochCount == 0n) {
+      return res.status(400).json({ code: 120, message: "Vault not start" });
+    }
+    const closed = await pc.readContract({ abi: abiBVault, address: bvault, functionName: "closed", blockNumber });
+    if (closed) {
+      return res.status(200).json({ data: [], timestamp: blockTime });
+    }
+    const users = await AppDS.getRepository(tables.event_bvault_swap)
+      .createQueryBuilder()
+      .select("user")
+      .distinct(true)
+      .where("address=:bvault AND epochId=:epochId AND block<=:block", { bvault, epochId: epochCount, block: blockNumber })
+      .getRawMany();
+    if (_.size(users) == 0) {
+      return res.status(200).json({ data: [], timestamp: blockTime });
+    }
+    const [lpAmount, yTotal, vaultYbalance] = await Promise.all([
+      pc.readContract({ abi: abiBVault, address: bvault, functionName: "assetBalance", blockNumber }),
+      pc.readContract({ abi: abiBVault, address: bvault, functionName: "yTokenTotalSupply", args: [epochCount], blockNumber }),
+      pc.readContract({ abi: abiBVault, address: bvault, functionName: "yTokenUserBalance", args: [epochCount, bvault], blockNumber }),
+    ]);
+    const yCirculAmount = yTotal - vaultYbalance;
+    const data =
+      yCirculAmount > 0n
+        ? await Promise.all(
+            users.map((item) =>
+              pc
+                .readContract({ abi: abiBVault, address: bvault, functionName: "yTokenUserBalance", args: [epochCount, item.user], blockNumber })
+                .then((userYBalance) => ({ address: item.user, balance: ((lpAmount * userYBalance) / yCirculAmount).toString() }))
+            )
+          )
+        : [];
+
+    return res.status(200).json({ data: data, timestamp: blockTime });
+  } catch (error) {
+    console.error("ir_points_program:", req.params["bvault"], req.params["block"], error);
+    return res.status(500).json({ code: 120, message: "Please retry a wait moment" });
+  }
+});
