d2946675ec19a8e19ac5bcb0c77a2a067356fb01
nonolook <looklooknono@outlook.com>
2025-08-22 10:09:40 +0800

    [all] fix build

--- schedule/utils.ts
+++ schedule/utils.ts
@@ -3,14 +3,14 @@ import { getPC } from "@/lib/publicClient";
 import { bigintMin } from "@/lib/utils";
 
 
-export async function getIndexEventParams(chainId: number, name: string, defChunk: bigint = 10000n, defStart: bigint = 0n) {
+export async function getIndexEventParams(chainId: number, name: string, defChunk: bigint = 10000n, defStart: bigint = 0n, cacheTime: number = 5000) {
   const pc = getPC(chainId);
   const start = (await getIndexConfig(name, defStart)) + 1n;
   const chunk = (await getIndexConfig(`event_block_chunk`, defChunk)) - 1n;
   if (chunk <= 0n) {
     return;
   }
-  const blockNumber = await pc.getBlockNumber({ cacheTime: 5000 });
+  const blockNumber = await pc.getBlockNumber({ cacheTime });
   const end = bigintMin([start + chunk, blockNumber]);
   if (end <= start) {
     return;
